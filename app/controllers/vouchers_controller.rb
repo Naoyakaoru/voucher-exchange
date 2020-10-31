e5528ca6acf4e789a36d80ffcd6cd8e178b552e2
Kelsey.Chen <naoyakaoru@gmail.com>
2020-10-31 18:04:38 +0800

    add index action for voucher, add basic view for voucher index

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -1,13 +1,20 @@
 class VouchersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @vouchers = current_user.vouchers
+  end
+
   def new
     @voucher = Voucher.new
   end
 
   def create
-    @voucher = Voucher.new(voucher_params)
+    @voucher = current_user.vouchers.new(voucher_params)
     if  @voucher.save
+      redirect_to vouchers_path, notice: "已成功兌換，您的兌換券序號為"
     else
-     render :new
+      render :new
     end 
   end
 
