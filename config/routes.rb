1ce9a6b2115c57c21a0d3f80cf330dc9bbf91659
Kelsey.Chen <naoyakaoru@gmail.com>
2020-10-31 12:13:40 +0800

    closing omni auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, 
     controllers: {
     confirmations: 'users/confirmations',
-    omniauth_callbacks: 'users/omniauth_callbacks',
     passwords: 'users/passwords',
     registrations: 'users/registrations',
     sessions: 'users/sessions',
