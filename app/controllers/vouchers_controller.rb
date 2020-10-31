bef2756f632e82e28925c5c2768f9f4499f16e5d
Kelsey.Chen <naoyakaoru@gmail.com>
2020-10-31 14:34:07 +0800

    modify vouchers controller new and create action

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -1,7 +1,18 @@
 class VouchersController < ApplicationController
   def new
+    @voucher = Voucher.new
   end
-  
+
   def create
+    @voucher = Voucher.new(voucher_params)
+    if  @voucher.save
+    else
+     render :new
+    end 
+  end
+
+  private
+  def voucher_params
+    params.require(:voucher).permit(:name, :tax_id, :tel)
   end
 end
