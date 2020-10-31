a0be6704aedd0eb29ab86a99fbe75f317cad3358
Kelsey.Chen <naoyakaoru@gmail.com>
2020-10-31 17:13:16 +0800

    let voucher belongs to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_31_062142) do
+ActiveRecord::Schema.define(version: 2020_10_31_065223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2020_10_31_062142) do
     t.string "tel"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_vouchers_on_user_id"
   end
 
 end
