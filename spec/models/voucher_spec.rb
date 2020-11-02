2c30ca656f0c55370ec271504d04fd6b50c91d64
Kelsey.Chen <naoyakaoru@gmail.com>
2020-11-02 15:24:51 +0800

    add test of already taken tax_id

--- spec/models/voucher_spec.rb
+++ spec/models/voucher_spec.rb
@@ -47,4 +47,24 @@ RSpec.describe Voucher, type: :model do
     expect(voucher2.errors[:tax_id]).to include("您所填寫的統一編號有誤，請確認後重新輸入")
     expect(voucher3.errors[:tax_id]).to include("統一編號請輸入8碼數字")
   end
+  it "is invalid if tax_id is already taken" do
+    user = User.new(
+      email: Faker::Internet.email,
+      password: '123123'
+    )
+    voucher1 = user.vouchers.new(
+      name: Faker::Company.name,
+      tax_id: '54191940',
+      tel: Faker::PhoneNumber.phone_number 
+    )
+    voucher2 = user.vouchers.new(
+      name: Faker::Company.name,
+      tax_id: '54191940',
+      tel: Faker::PhoneNumber.phone_number 
+    )
+    voucher1.valid?
+    voucher2.valid?
+    expect(voucher1).to be_valid
+    expect(voucher2.errors[:tax_id]).to eq([]) #錯誤寫在controller
+  end
 end
