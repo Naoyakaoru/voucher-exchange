be5f2076db4245faa34221a7a8e173a47951219e
Kelsey.Chen <naoyakaoru@gmail.com>
2020-11-02 13:45:45 +0800

    rspec can test tax_id of voucher, presence of voucher's every inputs

--- spec/models/voucher_spec.rb
+++ spec/models/voucher_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Voucher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name, tax_id(rules followed), tel" do
+    user = User.new(
+      email: Faker::Internet.email,
+      password: '123123'
+    )
+    voucher1 = user.vouchers.new(
+      name: Faker::Company.name,
+      tax_id: '54191940',
+      tel: Faker::PhoneNumber.phone_number 
+    )
+    voucher2 = user.vouchers.new(
+      name: Faker::Company.name,
+      tax_id: '54178279', #第7碼為7
+      tel: Faker::PhoneNumber.phone_number 
+    )
+    expect(voucher1).to be_valid
+    expect(voucher2).to be_valid
+  end
+
+  it "is invalid with a rules not followed tax_id" do
+    user = User.new(
+      email: Faker::Internet.email,
+      password: '123123'
+    )
+    voucher1 = user.vouchers.new(
+      name: Faker::Company.name,
+      tax_id: '',
+      tel: Faker::PhoneNumber.phone_number 
+    )
+    voucher2 = user.vouchers.new(
+      name: Faker::Company.name,
+      tax_id: '11111111',
+      tel: Faker::PhoneNumber.phone_number 
+    )
+    voucher3 = user.vouchers.new(
+      name: Faker::Company.name,
+      tax_id: '1111',
+      tel: Faker::PhoneNumber.phone_number 
+    )
+    voucher1.valid?
+    voucher2.valid?
+    voucher3.valid?
+    expect(voucher1.errors[:tax_id]).to include("統一編號不能為空白")
+    expect(voucher2.errors[:tax_id]).to include("您所填寫的統一編號有誤，請確認後重新輸入")
+    expect(voucher3.errors[:tax_id]).to include("統一編號請輸入8碼數字")
+  end
 end
